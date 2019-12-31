2eda3bd6bdfe1f586f8e5a88cd9e7e344577d0bd
andybeet <22455149+andybeet@users.noreply.github.com>
2019-12-31 12:53:52 -0500

    USER FIX

--- _deploy.sh
+++ _deploy.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
+[ -z "${GITHUB_TOKEN}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "Sarah.Gaichas@noaa.gov"
-git config --global user.name "Sarah Gaichas"
+git config --global user.email "andrew.beet@noaa.gov"
+git config --global user.name "andybeet"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+git clone -b gh-pages https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
